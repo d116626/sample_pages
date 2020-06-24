3811b1b15776f587cdec3f7f96964ceb824a8686
d116626 <d116626@gmail.com>
2020-06-23 23:27:06 -0300

    all

---
+++ pages/indicadores_vale/pages/educacao/js/get_values_educacao.js
@@ -0,0 +1,13 @@
+function getSelectValue_educacao() {
+    var selectorIndicator = document.getElementById("selectorIndicator").value;
+
+    var pathValue = `images/bars/${selectorIndicator}.html`;
+    var pathValue_maps = `images/maps/${selectorIndicator}.png`;
+
+    // var res = `${str1} ${str2}`;               
+    document.getElementById("iframeId_bars").src = pathValue;
+    document.getElementById("iframeId_maps").src = pathValue_maps;
+
+    console.log(pathValue);
+}
+getSelectValue_educacao();
